b201aa22e16d84b82afd7dcddc0a4f232427eecd
dungvankien <vankienhue@gmail.com>
2022-07-09 16:20:38 +0700

    add fix display prime

--- Loop-in-Java/src/DisplayPrimeNumber.java
+++ Loop-in-Java/src/DisplayPrimeNumber.java
@@ -2,45 +2,48 @@ import java.util.Scanner;
 
 public class DisplayPrimeNumber {
     public static void main(String[] args) {
-        int count=0;
+        int count = 0;
         int myNumber;
-        int i=2;
+        int i = 2;
         int j;
         boolean flag;
 
-        Scanner input=new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         System.out.println("Enter the number of primes!");
-        myNumber=input.nextInt();
-        while (count<myNumber){
-            flag=true;
-            for(j=2;j<=Math.sqrt(i);j++){
-                if(i%j==0){
-                    flag=false;
+        myNumber = input.nextInt();
+
+        while (count < myNumber) {
+            flag = true;
+            for (j = 2; j <= Math.sqrt(i); j++) {
+                if (i % j == 0) {
+                    flag = false;
                     break;
                 }
+
             }
-            if(flag){
+            if (flag) {
                 count++;
-                System.out.print(" "+ i);
+                System.out.print(" " + i);
             }
-            i+=1;
+            i += 1;
         }
+
         System.out.println("\n Primes Number less than 100!");
-        i=2;
-        while (count<100){
-            count=0;
-            flag=true;
-            for(j=2;j<=Math.sqrt(i);j++){
-                if(i%j==0){
-                    flag=false;
+
+        i = 2;
+        while (i < 100) {
+            flag = true;
+            for (j = 2; j <= Math.sqrt(i); j++) {
+                if (i % j == 0) {
+                    flag = false;
                     break;
                 }
             }
-            if(flag && i<100){
-                count=i;
-                System.out.print(" "+ i);
+
+            if (flag) {
+                System.out.print(" " + i);
             }
-            i+=1;
+            i += 1;
         }
     }
 }
