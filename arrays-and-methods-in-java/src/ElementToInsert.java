ff158e24179b029f696d0fc7c25fe86f5281dac1
dungvankien <vankienhue@gmail.com>
2022-07-11 16:39:39 +0700

    add fix

--- arrays-and-methods-in-java/src/ElementToInsert.java
+++ arrays-and-methods-in-java/src/ElementToInsert.java
@@ -2,31 +2,35 @@ import java.util.Scanner;
 
 public class ElementToInsert {
     public static void main(String[] args) {
-        int[] myArray={1,4,5,6,7,3,4,2,9};
+        int[] myArray = {1, 4, 5, 6, 7, 3, 4, 2, 9};
         int[] newArray;
-        Scanner input=new Scanner(System.in);
-        System.out.println("index from: "+0+" to "+(myArray.length-1));
+        Scanner input = new Scanner(System.in);
+        System.out.println("index from: " + 0 + " to " + (myArray.length));
         System.out.println("Enter Element:");
-        int element=input.nextInt();
-        System.out.println("Enter index:");
-        int index=input.nextInt();
-        newArray=new int[(myArray.length+1)];
-        for(int i=0,j=0;i< myArray.length;i++,j++){
-            if(index==i){
-             newArray[j]=element;
-             j++;
-             newArray[j]=myArray[i];
-            } else {
-                newArray[j]=myArray[i];
+        int element = input.nextInt();
+        int index;
+        do {
+            System.out.println("Enter index:");
+            index = input.nextInt();
+        } while (index < 0 || index > myArray.length);
+        newArray = new int[(myArray.length + 1)];
+        for (int i = 0, j = 0; i < myArray.length; i++, j++) {
+            if (index == i) {
+                newArray[j] = element;
+                j++;
+            }
+            newArray[j] = myArray[i];
+            if (index == myArray.length) {
+                newArray[index] = element;
             }
         }
         System.out.println("Old Array");
-        for(int item:myArray){
-            System.out.print(item+"\t");
+        for (int item : myArray) {
+            System.out.print(item + "\t");
         }
         System.out.println("\nNew Array");
-        for(int item:newArray){
-            System.out.print(item+"\t");
+        for (int item : newArray) {
+            System.out.print(item + "\t");
         }
     }
 }
