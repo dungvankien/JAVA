e32872ddf0fd57b8a77738aa7c4163e12557b24a
dungvankien <vankienhue@gmail.com>
2022-07-12 14:32:36 +0700

    add bubbleSort

---
+++ Sorting Algorithms/src/BubbleSort.java
@@ -0,0 +1,22 @@
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] list={2, 3, 2, 5, 6, 1, -2, 3, 14, 12};
+        System.out.println("Show the original list");
+        for (int item:list){
+            System.out.print(item +"\t");
+        }
+        int temp;
+        for(int i=0;i< list.length;i++){
+            for(int j= (list.length-1);j>i;j--)
+                if (list[j] < list[j - 1]) {
+                    temp = list[j - 1];
+                    list[j - 1] = list[j];
+                    list[j] = temp;
+                }
+        }
+        System.out.println("\nShow list after sorting");
+        for (int item:list) {
+            System.out.print(item+"\t");
+        }
+    }
+}
