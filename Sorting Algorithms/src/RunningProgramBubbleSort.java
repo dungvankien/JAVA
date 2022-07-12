2c8452cc98de35b0629c9fced7ec5f2d7eb9fc7d
dungvankien <vankienhue@gmail.com>
2022-07-12 14:55:57 +0700

    add stepProgram

--- Sorting Algorithms/src/RunningProgramBubbleSort.java
+++ Sorting Algorithms/src/RunningProgramBubbleSort.java
@@ -1,4 +1,4 @@
-public class BubbleSort {
+public class RunningProgramBubbleSort {
     public static void main(String[] args) {
         int[] list = {2, 3, 2, 5, 6, 1, -2, 3, 14, 12};
         System.out.println("Show the original list");
@@ -6,17 +6,19 @@ public class BubbleSort {
             System.out.print(item + "\t");
         }
         int temp;
+        int step = 0;
         for (int i = 0; i < list.length; i++) {
-            for (int j = (list.length - 1); j > i; j--)
+            for (int j = list.length - 1; j > i; j--) {
                 if (list[j] < list[j - 1]) {
                     temp = list[j - 1];
                     list[j - 1] = list[j];
                     list[j] = temp;
+                    System.out.println("\nProgram run step " + (step += 1) + ": ");
+                    for (int item : list) {
+                        System.out.print(item + "\t");
+                    }
                 }
-        }
-        System.out.println("\nShow list after sorting");
-        for (int item : list) {
-            System.out.print(item + "\t");
+            }
         }
     }
 }
