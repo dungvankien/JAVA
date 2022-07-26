bb26770c988aebc138af6471004fd77e3c390c54
dungvankien <vankienhue@gmail.com>
2022-07-26 17:30:38 +0700

    add fix colorable

--- Abstrac_Class_Interface/src/interfaceColorable/MainTestColorable.java
+++ Abstrac_Class_Interface/src/interfaceColorable/MainTestColorable.java
@@ -7,13 +7,14 @@ import comparable.Square;
 
 public class MainTestColorable {
     public static void main(String[] args) {
-        Shape[] shapes=new Shape[3];
-        shapes[0]= new Circle(4);
-        shapes[1]= new Rectangle(4,2);
-        shapes[2]= new Square(6);
-        for (Shape shape:shapes) {
+        Shape[] shapes = new Shape[3];
+        shapes[0] = new Circle(4);
+        shapes[1] = new Rectangle(4, 2);
+        shapes[2] = new Square(6);
+        for (Shape shape : shapes) {
             System.out.println(shape);
-            if(shape instanceof Square ){
+            if (shape instanceof Square) {
+                System.out.println(((Square) shape).getArea());
                 ((Square) shape).howToColor();
             }
         }
