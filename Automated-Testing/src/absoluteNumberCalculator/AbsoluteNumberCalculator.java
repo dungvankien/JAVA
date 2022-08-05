daa7685fbc0c0a54d4a51eaf944010be88c6ef1d
dungvankien <vankienhue@gmail.com>
2022-08-05 10:26:34 +0700

    add Test

---
+++ Automated-Testing/src/absoluteNumberCalculator/AbsoluteNumberCalculator.java
@@ -0,0 +1,10 @@
+package absoluteNumberCalculator;
+
+public class AbsoluteNumberCalculator {
+    public static int finAbsolute(int number){
+        if(number<0){
+            return -number;
+        }
+        return number;
+    }
+}
---
+++ Automated-Testing/src/absoluteNumberCalculator/AbsoluteNumberCalulatorTest.java
@@ -0,0 +1,34 @@
+package absoluteNumberCalculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AbsoluteNumberCalulatorTest {
+    @Test
+    void testFindAbsolute0() {
+        int number = 0;
+        int expected = 0;
+
+        int result = AbsoluteNumberCalculator.finAbsolute(number);
+        assertEquals (expected, result);
+    }
+
+    @Test
+    void testFindAbsolute1() {
+        int number = 1;
+        int expected = 1;
+
+        int result = AbsoluteNumberCalculator.finAbsolute(number);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void testFindAbsoluteNegative1() {
+        int number = -1;
+        int expected = 1;
+
+        int result = AbsoluteNumberCalculator.finAbsolute(number);
+        assertEquals(expected, result);
+    }
+}
